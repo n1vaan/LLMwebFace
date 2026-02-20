986d314011f54bb4c8032b9a9bcf147e189f29b3
n1vaan <nivahn@gmail.com>
2026-02-20 13:30:37 -0500

    First

---
+++ app.py
@@ -0,0 +1,19 @@
+from ollama import chat
+from flask import Flask
+
+app = Flask(__name__)
+
+def generate_response(prompt, model="llama3"):
+    fullResponse = ""
+    stream = chat(
+        model=model,
+        messages=[{"role": "user", "content": prompt}],
+        stream=True
+    )
+    
+    for chunk in stream:
+        token = chunk['message']['content']
+        if token:
+            fullResponse += token
+            yield token, fullResponse  
+
